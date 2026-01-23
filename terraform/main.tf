06059455832ce2e0ce3601e70a87830934d44c08
Semyon Levin <levin.semen@gmail.com>
2026-01-24 01:06:33 +0400

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,9 +35,10 @@ resource "google_secret_manager_secret_iam_member" "secret_accessor" {
 
 # Cloud Run service
 resource "google_cloud_run_v2_service" "github_token_issuer" {
+  deletion_protection = false
   #name     = "github-repository-token-issuer"
-  name                = "gh-repo-token-issuer"
-  location            = var.region
+  name     = "gh-repo-token-issuer"
+  location = var.region
 
   template {
     service_account = google_service_account.cloud_run_sa.email
